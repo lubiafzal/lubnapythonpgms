54c63bfc28abc42519edd3cb6445d38deb4ce76f
lubna <lubi4af@gmail.com>
2022-06-06 11:30:30 +0530

    add list pgm

---
+++ listworks/list.py
@@ -0,0 +1,77 @@
+# expenses=[12000,15000,23000,34000,45000]
+# print(expenses[0])
+
+
+# numbers=[12,34,56,78,98,76,45,23,21]
+# print(numbers)
+
+# for i in range(0,10):
+#     print(numbers[i])
+
+# for num in numbers:
+#     print(num)
+
+# for i in range(0,len(numbers)):
+#     print(numbers[i])
+
+# numbers=[12,13,14,15,16,17,18,19,20]
+# for num in numbers:
+#     if num%2==0:
+#         print(num)
+
+# numbers=[12,13,14,15,16,17,18,19,20]
+
+# for num in numbers:
+#     if num==15:
+#         print(num)
+#     elif num>15:
+#         print(num+1)
+#     elif num<15:
+#         print(num-1)
+# numbers=[12,13,14,15,16,17,18,19,20]
+# for num in numbers:
+#       print(len(numbers[14,18]))
+
+# numbers=[-1,2,0,-3,-4,5,-2,0,3,-4,-5,0,-7,8]
+# count=0
+# a=0
+# b=0
+# for num in numbers:
+#     if num<0:
+#         count+=1
+#     elif num>0:
+#         a+=1
+#     else:
+#         b+=1
+
+# print(f" number of positive numbers {a}")
+# print(f" number of negative numbers {count}")
+# print(f" number of zero numbers {b}")
+# sum=0
+# numbers=[1,2,0,3,4,5,2,0,3,4,5,0,7,8]
+# for num in numbers:
+#     sum+=num
+# print(sum)
+
+# num=[12,13,14,15,16,17,18,19,20]
+# print(len(num[14,19]))
+
+arr=[1,2,3,13,14,15,16,65,78,89]
+element=14
+
+for n in arr:
+    n == element
+print(f"element found")
+
+
+
+
+
+
+
+
+
+
+
+
+
