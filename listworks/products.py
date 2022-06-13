d035aa38638b3d50ff0038492bad8b3e9ec5297a
lubna <lubi4af@gmail.com>
2022-06-13 23:20:49 +0530

    products work done

--- listworks/products.py
+++ listworks/products.py
@@ -1,7 +1,7 @@
 mobiles = [
     [1000, "samsungA52", "4g", "AMOLED", 27000, "samsung", 12],
     [1001, "samsungA52s", "5g", "AMoLED", 32000, "samsung", 20],
-    [1002, "redminote10", "4g", "led", 17000, "redmi", 50],
+    [1002, "redminote10", "4g", "led", 10000, "redmi", 50],
     [1003, "redminote11pro", "5g", "superAMOLED", 20000, "redmi", 70],
     [1004, "samsungA72", "5g", "AMOLED", 27000, "samsung", 1],
     [1005, "samsungA53", "5g", "AMOLED", 27000, "samsung", 34],
@@ -11,35 +11,48 @@ mobiles = [
     [1009, "iphone13", "5g", "AMOLED", 97000, "apple", 0],
     [1010, "oneplusnordce2", "5g", "AMOLED", 23000, "oneplus", 67]
 ]
-print(f"total no of mobiles= {len(mobiles)}")
-# available no of mobiles
+
+# no of mobiles
+print(f"no of mobiles = {len(mobiles)}")
+# total no of available mobiles
 available=[mob for mob in mobiles if mob[-1]!=0]
-print(f"total no of available mobiles={len(available)}")
+print(f"available mobiles={len(available)}")
+
 # total no of out of stock mobiles
-out_stock=(([mob for mob in mobiles if mob[-1]==0 ]))
-print(f"number of out of stock mobiles={len(out_stock)}")
+out_stock=[mob for mob in mobiles if mob[-1]==0]
+print(f"total no of out of stock mobiles{len(out_stock)}")
 # total stock
-total_stock=[mob[-1] for mob in mobiles if mob[-1]!=0]
-print(f"total stock of mobiles = {sum(total_stock)}")
-# print mobiles btwn 20 to 30
-twenty_to_thirty=print([mob for mob in mobiles if mob[4]>20000 and mob[4]<30000])
+stock=[mob[-1] for mob in mobiles]
+print(f"total no of  mobiles={sum(stock)}")
+# print mob in 20 to 30k
+twenty_thirty=print([mob for mob in mobiles if mob[-3]>=20000 and mob[-3]<=30000])
+twenty_thirty=print([mob  for mob in mobiles if mob[-3] in range(20000,30001)])
 # print all 5g mobiles
-five_g_mob=print([mob for mob in mobiles if mob[2]=="5g"])
-# print all samsung phones
-samsung_mobiles=print([mob for mob in mobiles if mob[-2]=="samsung"])
-# print costly mob
-costly_mob=sorted([mob for mob in mobiles ])
-print(costly_mob[-1])
-# print all mobiles having stock gt 10
-stock_gt_10=([mob for mob in mobiles if mob[-1]>10])
-print(stock_gt_10)
-# count of mobiles having display amoled
-display_amoled=([mob for mob in mobiles if mob[3]=="AMOLED"])
-print(f"number of mobiles with display amoled {len(display_amoled)}")
-# sort the mobiles based on price in descending order
-price_mob=([mob for mob in mobiles ])
-print(sorted(price_mob[-3],reverse=True))
+five_g=print([mob for mob in mobiles if mob[2]=="5g"])
+# print all samsung mobiles
+samsung=print([mob for mob in mobiles if mob[-2]=="samsung"])
+# print costly mobile
+max_price=(max([mob[4] for mob in mobiles]))
+costly=[mob for mob in mobiles if max_price == mob[4]]
+print(costly)
+# print low cost mob
+low_cost=min([mob[4] for mob in mobiles])
+minimum=[mob for mob in mobiles if low_cost==mob[4]]
+print(minimum)
+# print all mobiles haing stock grt 10
+stock_grt_ten=print([mob for mob in mobiles if mob[-1]>10])
+# sort mobiles based on price
+mobiles.sort(reverse=True,key=lambda mob:mob[-3])
+print(mobiles)
+# sort according to availability
+mobiles.sort(reverse=True,key=lambda mob:mob[-1])
+print(mobiles)
+# is there any mobile for 10000
+vc=[True for mob in mobiles if mob[-3]==60000]
+print(vc)
 
-# sort the mobile based on availability
+mx_mob=max(mobiles,key=lambda mob:mob[-3])
+print(mx_mob)
 
-
+mn_lowest=min(mobiles,key=lambda mob:mob[-3])
+print(mn_lowest)
---
+++ tuple/item_assignment.py
@@ -0,0 +1,3 @@
+tuple=(12,13,"error",True,32000)
+print(tuple)
+tuple[-1]=789
